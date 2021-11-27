de53c0bb3e058b12d31363037e4b8e0dfebe269a
Sarvar Saidov <saidov1101119@gmail.com>
2021-11-27 01:22:44 -0600

    just added test comment

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
-
+//new edit here
 const Banner = ({
   purpose,
   title1,
