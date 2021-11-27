367cf39923633d36d6d643001d6f3089a39286e3
Sarvar Saidov <saidov1101119@gmail.com>
2021-11-27 01:10:18 -0600

    Started working on Real_estate app
    It gets the for sale and for rent information from rapidApi and renders them in the main page

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+//function that accepts url to fetch
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "d93c9fcd08msh9e6677eabcf2855p1539dejsnb7590da107b5",
+    },
+  });
+  return data;
+};
