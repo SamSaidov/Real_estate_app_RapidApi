d0b47faa1cc5f46959852390abb38d22f8ca25c9
Sarvar Saidov <saidov1101119@gmail.com>
2022-01-21 23:04:15 -0600

    updated

--- pages/search.js
+++ pages/search.js
@@ -11,7 +11,6 @@ import noresult from "../assets/noresult.svg";
 import { fetchApi, baseUrl } from "../utils/fetchApi";
 
 // searching functionality
-
 const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
